79792b3bd5fd4e58ac0ef3434fe802edc456a478
Natalya M <natalyamerr@gmail.com>
2025-01-20 03:14:39 +0300

    fix: remove redundant process module call

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,4 +4,4 @@ const fs = require('fs');
 const { stdout } = require('process');
 const fileLocation = path.join(__dirname, 'text.txt');
 const readStream = fs.createReadStream(fileLocation, 'utf-8');
-readStream.on('data', function (chunk) { process.stdout.write(chunk); });
+readStream.on('data', function (chunk) { stdout.write(chunk); });
