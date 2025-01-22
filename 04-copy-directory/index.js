a5c09c843cb275467272b344aafafac8b5e8ac09
Natalya M <natalyamerr@gmail.com>
2025-01-23 02:23:28 +0300

    feat: implement functionality to copy files in 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,3 +5,11 @@ const folderPath = path.join(__dirname, '/files');
 const destFolder = path.join(__dirname, '/files-copy');
 
 fs.mkdir(destFolder, { recursive: true });
+const files = fs.readdir(folderPath, { withFileTypes: true });
+files.then((files) => {
+    files.forEach((file) => {
+        fs.copyFile(folderPath + '/' + file.name,
+            destFolder + '/' + file.name,
+            fs.constants.COPYFILE_EXCL)
+    })
+})
