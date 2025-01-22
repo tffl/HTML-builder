fe44a751bf7c927d86750b3af4d4b839e369ae50
Natalya M <natalyamerr@gmail.com>
2025-01-23 02:32:44 +0300

    feat: add 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,6 +5,17 @@ const folderPath = path.join(__dirname, '/files');
 const destFolder = path.join(__dirname, '/files-copy');
 
 fs.mkdir(destFolder, { recursive: true });
+const filesToDelete = fs.readdir(destFolder, { withFileTypes: true });
+filesToDelete.then((files) => {
+    if (files.length === 0) {
+        return
+    }
+    files.forEach((file) => {
+        fs.unlink(destFolder + '/' + file.name);
+    })
+})
+
+
 const files = fs.readdir(folderPath, { withFileTypes: true });
 files.then((files) => {
     files.forEach((file) => {
